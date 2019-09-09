7dbbb771ca4c0abed022d8ce7fa2077b065c0106
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-09-09 13:34:00 -0700

    Update FahrenheitToCelsius.cpp

--- FahrenheitToCelsius.cpp
+++ FahrenheitToCelsius.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main()
 {
-  short fahrenheit, celsius;
+	short fahrenheit, celsius, five = 5, nine = 9;
 
-  cout << "Enter temperature in Fahrenheit: ";
-  cin >> fahrenheit;
+	cout << "Enter temperature in Fahrenheit: ";
+	cin >> fahrenheit;
 
-  _asm
-  {
-    mov eax, fahrenheit; //eax = fahrenheit
-    sub eax, 32;         //eax = fahrenheit-32
-    imul 5;              //eax = 5(fahrenheit-32)
-    idiv 9;              //eax = 5(fahrenheit-32)/9
-    mov celsius, eax;    //celsius = eax = 5(fahrenheit-32)/9
-  }
+	_asm
+	{
+		mov ax, fahrenheit; //ax = fahrenheit
+		sub ax, 32;         //ax = fahrenheit-32
+		imul five;          //ax = 5(fahrenheit-32)
+		idiv nine;          //ax = 5(fahrenheit-32)/9
+		mov celsius, ax;    //celsius = ax = 5(fahrenheit-32)/9
+	}
 
-  cout << fahrenheit << ' ' << (char)167 << "F is " << celsius << ' ' << (char)167 << 'C'; //how's º or ⁰?
+	cout << fahrenheit << ' ' << (char)167 << "F is " << celsius << ' ' << (char)167 << 'C';
 
-  return 0;
+	return 0;
 }
