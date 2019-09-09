4553dbc533d4e3ba14cf854d7eb168e450991e27
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-09-09 13:26:51 -0700

    Update SquareAndTriangle.cpp
    imul 2; doesn't work
    
    Using "length" as a variable name wouldn't work.

--- SquareAndTriangle.cpp
+++ SquareAndTriangle.cpp
@@ -4,38 +4,37 @@ using namespace std;
 
 int main()
 {
-  int a, b, c, h, triangleArea, trianglePerimeter, 
-      length, width, rectangleArea, rectanglePerimeter;
+	int a, b, c, h, triangleArea, trianglePerimeter, l, w, rectangleArea, rectanglePerimeter, two = 2;
 
-  cout << "Enter the values of a, b, c, and h of your triangle (c is the base of the triangle, h is the height): ";
-  cin >> a >> b >> c >> h;
-  cout << "Enter the length and width of the rectangle: ";
-  cin >> length >> width;
+	cout << "Enter the values of a, b, c, and h of your triangle (c is the base of the triangle, h is the height): ";
+	cin >> a >> b >> c >> h;
+	cout << "Enter the length and width of the rectangle: ";
+	cin >> l >> w;
 
-  _asm
-  {
-    mov eax, a;            //eax = a
-    imul c;                //eax = ac
-    idiv 2;                //eax = ac/2
-    mov triangleArea, eax; //triangleArea = eax = ac/2
+	_asm
+	{
+		mov eax, c;            //eax = c
+		imul h;                //eax = ch
+		idiv two;              //eax = ch/2
+		mov triangleArea, eax; //triangleArea = eax = ch/2
 
-    mov eax, a;                 //eax = a
-    add eax, b;                 //eax = a+b;
-    add eax, c;                 //eax = a+b+c
-    mov trianglePerimeter, eax; //trianglePerimeter = eax = a+b+c
+		mov eax, a;                 //eax = a
+		add eax, b;                 //eax = a+b
+		add eax, c;                 //eax = a+b+c
+		mov trianglePerimeter, eax; //trianglePerimeter = eax = a+b+c
 
-    mov eax, length;        //eax = length
-    imul width;             //eax = length*width
-    mov rectangleArea, eax; //rectangleArea = eax = lenght*width
+		mov eax, l;        //eax = length
+		imul w;             //eax = length*width
+		mov rectangleArea, eax; //rectangleArea = eax = length*width
 
-    mov eax, length;             //eax = length
-    add eax, width;              //eax = length+width
-    imul 2;                      //eax = (length+width)*2
-    mov rectanglePerimeter, eax; //rectanglePerimeter = eax = (length+width)*2
-  }
+		mov eax, l;             //eax = length
+		add eax, w;              //eax = length+width
+		imul two;                    //eax = (length+width)*2
+		mov rectanglePerimeter, eax; //rectanglePerimeter = eax = (length+width)*2
+	}
 
-  cout << "Triangle\n\tArea\t" << triangleArea << "\n\tPerimeter\t" << trianglePerimeter
-       << "\nRectangle\n\tArea\t" << rectangleArea << "\nPerimeter\t" << rectanglePerimeter;
+	cout << "\nTriangle\n\tArea............" << triangleArea << "\n\tPerimeter......." << trianglePerimeter
+		 << "\nRectangle\n\tArea............" << rectangleArea << "\n\tPerimeter......." << rectanglePerimeter;
 
-  return 0;
+	return 0;
 }
