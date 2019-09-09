fc1774cf83d0b9cce6704489e2feeb8fe0ae25b6
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-09-09 14:01:45 -0700

    Update AddDigits.cpp
    There's probably a better way to make edx:eax = num than by using imul one;

--- AddDigits.cpp
+++ AddDigits.cpp
@@ -4,23 +4,27 @@ using namespace std;
 
 int main()
 {
-  int num, result; // ten = 10;
+	int num, result, ten = 10, one = 1;
 
-  cout << "Enter a 3-digit number: ";
-  cin >> num;
+	cout << "Enter a 3-digit number: ";
+	cin >> num;
 
-  _asm
-  {
-    mov eax, num;
-    idiv 10;
-    mov ebx, edx;
-    idiv 10;
-    add ebx, edx;
-    idiv 10;
-    add ebx, edx;
-  }
+	_asm
+	{
+		mov eax, num;    //eax = num
+		imul one;        //edx:eax = num
+		idiv ten;        //eax = num/10 & edx = num%10
+		mov ebx, edx;    //ebx = ones digit
+		imul one;        //edx:eax = num/10
+		idiv ten;        //eax = num/100 & edx = num/10%10
+		add ebx, edx;    //ebx = ones digit + tens digit
+		imul one;        //edx:eax = num
+		idiv ten;        //eax = num/1000 & edx = num/10/10%10
+		add ebx, edx;    //ebx = ones digit + tens digit + hundreds digit
+		mov result, ebx; //store in result
+	}
 
-  cout << "The sum of the digits in " << num << is << result;
+	cout << "The sum of the digits in " << num << " is " << result;
 
-  return 0;
+	return 0;
 }
