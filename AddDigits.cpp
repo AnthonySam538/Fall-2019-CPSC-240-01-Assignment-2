4cb5e3b26cd1ecd0e97ef04b00effb672ee66a7a
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-09-11 14:43:37 -0700

    Update AddDigits.cpp
    Forgot to remove the variable, "one," since we don't need it anymore.

--- AddDigits.cpp
+++ AddDigits.cpp
@@ -4,24 +4,32 @@ using namespace std;
 
 int main()
 {
-	int num, result, ten = 10, one = 1;
+	int num, result, ten = 10;
 
 	cout << "Enter a 3-digit number: ";
 	cin >> num;
 
 	_asm
 	{
-		mov eax, num;    //eax = num
-		cdq;             //edx:eax = num
-		idiv ten;        //eax = num/10 & edx = num%10
-		mov ebx, edx;    //ebx = ones digit
-		cdq;             //edx:eax = num/10
-		idiv ten;        //eax = num/100 & edx = num/10%10
-		add ebx, edx;    //ebx = ones digit + tens digit
-		cdq;             //edx:eax = num
-		idiv ten;        //eax = num/1000 & edx = num/10/10%10
-		add ebx, edx;    //ebx = ones digit + tens digit + hundreds digit
-		mov result, ebx; //store in result
+		mov eax, num; //eax = num
+
+		//get the ones digit
+		cdq;          //edx:eax = num
+		idiv ten;     //eax = num/10 & edx = num%10
+		mov ebx, edx; //ebx = ones digit
+
+		//get the tens digit
+		cdq;          //edx:eax = num/10
+		idiv ten;     //eax = num/100 & edx = num/10%10
+		add ebx, edx; //ebx = ones digit + tens digit
+
+		//get the hundreds digit
+		cdq;          //edx:eax = num
+		idiv ten;     //eax = num/1000 & edx = num/10/10%10
+		add ebx, edx; //ebx = ones digit + tens digit + hundreds digit
+
+		//store in result
+		mov result, ebx;
 	}
 
 	cout << "The sum of the digits in " << num << " is " << result;
